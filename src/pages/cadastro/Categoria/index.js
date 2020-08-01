817135146384056aaa37573f10314e74ed2e75ed
marianefelix <marianefelix59@gmail.com>
2020-08-01 12:16:16 -0300

    Alteração no link

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -31,18 +31,23 @@ function CadastroCategoria(){
     }
 
     useEffect( () => {
-        if (window.location.href.includes('localhost')) {
-            const URL = 'https://mariflix.herokuapp.com/categorias';
-            fetch(URL)
-              .then(async (respostaDoServer) => {
-                if (respostaDoServer.ok) {
-                  const resposta = await respostaDoServer.json();
-                  setCategorias(resposta);
-                  return;
-                }
-                throw new Error('Não foi possível pegar os dados');
-                });
-        }
+        
+        const URL = window.location.hostname.includes('localhost')
+            ? 'http://localhost:8080/categorias'
+            : 'https://mariflix.herokuapp.com/categorias';
+
+        fetch(URL)
+            .then(async (respostaDoServer) => {
+            if (respostaDoServer.ok) {
+                const resposta = await respostaDoServer.json();
+                setCategorias([
+                    ...resposta,
+                ]);
+                return;
+            }
+            throw new Error('Não foi possível pegar os dados');
+            });
+        
 
     }, []);
     
