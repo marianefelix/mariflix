31ddad4db441ce8cfaf1bb6a0d4480761197a57e
marianefelix <marianefelix59@gmail.com>
2020-08-01 11:50:11 -0300

    Link do servidor

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -32,7 +32,7 @@ function CadastroCategoria(){
 
     useEffect( () => {
         if (window.location.href.includes('localhost')) {
-            const URL = 'http://localhost:8080/categorias';
+            const URL = 'https://mariflix.herokuapp.com/categorias';
             fetch(URL)
               .then(async (respostaDoServer) => {
                 if (respostaDoServer.ok) {
